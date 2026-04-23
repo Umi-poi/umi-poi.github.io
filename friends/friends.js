584e0c3b9123c834946c825a376c41a759d9ee18
umi <2683418365@qq.com>
2026-04-23 11:48:07 +0800

    Site updated: 2026-04-23 11:48:06

---
+++ friends/friends.js
@@ -0,0 +1,19 @@
+const friendsData = {
+  friends: [
+    {
+      class: {
+        name: '小伙伴们',
+        desc: '这个分类下的友链配置了随机排列',
+        random: true
+      },
+      list: [
+        {
+          name: '000',
+          url: 'https://fridemn.best/',
+          avatar: 'assets/images/Hexo-Theme-MEOW.png',
+          desc: '旮旯game大神'
+        },
+      ]
+    },
+  ]
+};
